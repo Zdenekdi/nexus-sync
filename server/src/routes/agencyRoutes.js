d117622b41ae443422310167aa9b7f2ea1ff476a
Zdenekdi <dias.zd@gmail.com>
2026-03-24 10:39:43 +0100

    No changes were provided to summarize.

--- server/src/routes/agencyRoutes.js
+++ server/src/routes/agencyRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const agencyController = require('../controllers/agencyController');
+const profileController = require('../controllers/profileController');
 const authenticate = require('../middleware/authMiddleware');
 
 router.use(authenticate);
@@ -9,5 +10,6 @@ router.patch('/settings', agencyController.updateSettings);
 router.get('/settings', agencyController.getSettings);
 router.get('/users', agencyController.getUsers);
 router.get('/stats', agencyController.getStats);
+router.get('/profiles', profileController.getProfiles);
 
 module.exports = router;
